5aa95b4e61d8c6c3c578767e83e30071a9e81cff
Vitor Senise <vitor.senise@gmail.com>
2024-02-09 00:47:15 -0300

    feat - quick-union variations

---
+++ src/dynamic-connectivity/model/quick-union-weighted-with-path-compression/QuickUnionWeightedWithPathCompression.ts
@@ -0,0 +1,38 @@
+export class QuickUnion {
+  parents: number[];
+  weights: number[];
+
+  constructor(n: number) {
+    this.parents = Array.from({ length: n }, (_, i) => i);
+    this.weights = Array.from({ length: n }, (_, i) => 1);
+  }
+
+  private parent(a: number) {
+    while (this.parents[a] !== a) {
+      this.parents[a] = this.parents[this.parents[a]];
+      a = this.parents[a];
+    }
+    return a;
+  }
+
+  connected(a: number, b: number) {
+    return this.parent(a) === this.parent(b);
+  }
+
+  connect(a: number, b: number) {
+    const parentA = this.parent(a);
+    const parentB = this.parent(b);
+
+    if (parentA === parentB) {
+      return;
+    }
+
+    if (this.weights[parentA] < this.weights[parentB]) {
+      this.parents[parentA] = parentB;
+      this.weights[parentB] += this.weights[parentA];
+    } else {
+      this.parents[parentB] = parentA;
+      this.weights[parentA] += this.weights[parentB];
+    }
+  }
+}
